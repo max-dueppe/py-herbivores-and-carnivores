a0845e6a7fbc609cfe9be59f373f51a6d8e2581f
max-dueppe <max.dueppe@pm.me>
2026-01-30 20:20:22 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 class Animal:
-    alive: list[Animal] = []
+    alive: list["Animal"] = []
 
-    def __init__(self,
-                 name: str,
-                 health: int = 100) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100) -> None:
         self.name = name
         self.hidden = False
         self.health = health
@@ -23,8 +24,9 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    def bite(self,
-             animal: Animal) -> None:
+    def bite(
+            self,
+            animal: Animal) -> None:
         if animal.hidden or isinstance(animal, Carnivore):
             return
         animal.health -= 50
